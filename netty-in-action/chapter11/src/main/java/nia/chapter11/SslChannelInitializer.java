96d635ac96f8f4e61b7bd3d6ecc5f6427fda4f09
xuanjian <xuanjian@wacai.com>
2019-06-23 17:04:25 +0800

    netty in action chapter11 examples

---
+++ netty-in-action/chapter11/src/main/java/nia/chapter11/SslChannelInitializer.java
@@ -0,0 +1,31 @@
+package nia.chapter11;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * 11-1 添加 SSL/TLS 支持
+ *
+ * @author xuanjian
+ */
+public class SslChannelInitializer extends ChannelInitializer<Channel> {
+
+    private final SslContext context;
+
+    private final boolean startTls;
+
+    public SslChannelInitializer(SslContext context, boolean startTls) {
+        this.context = context;
+        this.startTls = startTls;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        SSLEngine engine = context.newEngine(ch.alloc());
+        ch.pipeline().addLast("ssl", new SslHandler(engine, startTls));
+    }
+}
